c98115025927938528e53f679643ea6040653965
leefell <alexandreaug21@gmail.com>
2023-06-22 21:27:48 -0300

    Create NomeScanner.java

---
+++ Exemplos Básicos/NomeScanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NomeScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome + "! Bem-vindo(a)!");
+
+        scanner.close();
+    }
+}
